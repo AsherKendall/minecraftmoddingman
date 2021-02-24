e0ae66a1c90bce3b32d1251b0f94dec1e2de593c
AsherKendall <36317228+asherkendall@users.noreply.github.com>
2021-02-24 17:17:59 -0600

    Add files via upload

---
+++ src/main/java/cubicoder/tutorialmod/init/ModItems.java
@@ -0,0 +1,12 @@
+package cubicoder.tutorialmod.init;
+
+import cubicoder.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@ObjectHolder(TutorialMod.MODID)
+public class ModItems {
+
+    public static final Item FIRST_ITEM = null;
+
+}
